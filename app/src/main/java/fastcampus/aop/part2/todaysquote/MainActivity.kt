fd3e9729bcb8aae9bb3a558d8422e941a9d91a00
sa1583 <sa1583@naver.com>
2022-03-10 13:42:03 +0900

    basic UI

--- app/src/main/java/fastcampus/aop/part2/todaysquote/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/todaysquote/MainActivity.kt
@@ -2,10 +2,32 @@ package fastcampus.aop.part2.todaysquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        viewPager.adapter = QuotesPagerAdapter(
+            listOf(
+                Quote(
+                    "나는 생각한다. 고로 나는 존자핸다",
+                    "데카르트"
+                ),
+                Quote(
+                    "아라리아라리",
+                    "누군가"
+                )
+            )
+        )
     }
 }
---
+++ app/src/main/java/fastcampus/aop/part2/todaysquote/Quote.kt
@@ -0,0 +1,6 @@
+package fastcampus.aop.part2.todaysquote
+
+data class Quote(
+    val quote: String,
+    val name: String
+)
